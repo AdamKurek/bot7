3fcd86ebb6ff46d658a7132f8184040c4facfee6
AdamKurek <kurek.adam1@gmail.com>
2024-04-21 11:24:33 +0200

    added youtube

--- Program.cs
+++ Program.cs
@@ -58,19 +58,19 @@ namespace bot7
         {
             if (user.IsBot)
             {
-                botsCannal = state2.VoiceChannel.Name;
+                botsCannal = state2.VoiceChannel?.Name;
                 return;
             }
-            if (state1.VoiceChannel.Name != state2.VoiceChannel.Name&& state2.VoiceChannel.Name == botsCannal)
+            if (state1.VoiceChannel is object ? state1.VoiceChannel.Name != state2.VoiceChannel.Name : true&& state2.VoiceChannel.Name == botsCannal)
             {
                 if (user.GlobalName == "MrLeon")
                 {
                     VoiceCommands.cancellationToken.Cancel();
                     await VoiceCommands.PlayOnce("C:/Users/kurek/Documents/lemon.mp3");
-                    await VoiceCommands.SendAsync(VoiceCommands.audioClient, "C:/Users/kurek/Documents/lemon.mp3", VoiceCommands.cancellationToken.Token);
+                    //await VoiceCommands.SendAsync(VoiceCommands.audioClient, "C:/Users/kurek/Documents/lemon.mp3", VoiceCommands.cancellationToken.Token);
                     return;
                 }
-                if (user.GlobalName == "! Vicky")
+                if (user.Username == "! Vicky")
                 {
                     await VoiceCommands.PlayOnce("C:/Users/kurek/Documents/viczki.mp3");
                     return;
@@ -90,7 +90,7 @@ namespace bot7
                     await VoiceCommands.PlayOnce("C:/Users/kurek/Documents/krzys.mp3");
                     return;
                 }
-                if (user.GlobalName == "Robul")
+                if (user.GlobalName == "!Dawid Bombuszczak") 
                 {
                     await VoiceCommands.PlayOnce("C:/Users/kurek/Documents/robul.mp3");
                     return;
--- VoiceCommands.cs
+++ VoiceCommands.cs
@@ -9,6 +9,10 @@ using Discord.WebSocket;
 using Discord.Audio;
 using System.Diagnostics;
 using System.Threading;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using System.IO;
+
 
 namespace bot7
 {
@@ -19,8 +23,10 @@ namespace bot7
         public static IAudioClient audioClient;
         public static Thread thread;
         public static bool imStopping = false;
+        private static YoutubeClient youtube = new YoutubeClient();
+
         [Command("play")]
-        public async Task PlayCommand()
+        public async Task PlayCommand(string url = "https://www.youtube.com/watch?v=woNw5Dyqhzo")
         {
             if (Context.User is SocketGuildUser guildUser)
             {
@@ -38,7 +44,7 @@ namespace bot7
                                  {
                                      Console.WriteLine("proboje");
                                      var token = cancellationToken.Token;
-                                    await SendAsync(audioClient, "C:/Users/kurek/Documents/melon.mp3", token);
+                                     await SendAsyncYT(audioClient, "https://www.youtube.com/watch?v=woNw5Dyqhzo", token);
                                      if (token.IsCancellationRequested)
                                      {
                                          return;
@@ -81,27 +87,26 @@ namespace bot7
                 thread = new(async () => {
                     try
                     {
-                            cancellationToken = new();
-                            var tkn = cancellationToken.Token;
-                            await SendAsync(audioClient, pathto, tkn);
-                            if (tkn.IsCancellationRequested)
+                        cancellationToken = new();
+                        var tkn = cancellationToken.Token;
+                        await SendAsync(audioClient, pathto, tkn);
+                        if (tkn.IsCancellationRequested)
+                        {
+                            return;
+                        }
+                    while (true)
+                        {
+                            var token = cancellationToken.Token;
+                            //await SendAsync(audioClient, "C:/Users/kurek/Documents/melon.mp3", token);
+                            
+                            await SendAsyncYT(audioClient, "https://www.youtube.com/watch?v=woNw5Dyqhzo", token);
+
+                            if (token.IsCancellationRequested)
                             {
                                 return;
                             }
-
-
-                        while (true)
-                            {
-                                var token = cancellationToken.Token;
-                                await SendAsync(audioClient, "C:/Users/kurek/Documents/melon.mp3", token);
-                                if (token.IsCancellationRequested)
-                                {
-                                    return;
-                                }
-                            }
-
-
                         }
+                    }
                     catch (Exception e)
                     {
                         Console.WriteLine("it " + e.Message);
@@ -122,29 +127,83 @@ namespace bot7
                 return null!;
             }
             private static Process CreateStream(string path)
-        {
-            //Console.WriteLine($"trying to get the process");
+            {
+                //Console.WriteLine($"trying to get the process");
 
-            if (_currentProcess != null )
+                if (_currentProcess != null)
+                {
+                    _currentProcess.Dispose();
+                }
+
+#pragma warning disable CS8603 // Possible null reference return.
+                return Process.Start(new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true, 
+                });
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        private async static Task<string> CreateStreamYT(string url)
+        {
+
+            if (_currentProcess != null)
             {
                 _currentProcess.Dispose();
             }
+            try {
 
-#pragma warning disable CS8603 // Possible null reference return.
-            return Process.Start(new ProcessStartInfo
-            {
-                FileName = "ffmpeg",
-                Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
-                UseShellExecute = false,
-                RedirectStandardOutput = true, 
-            });
-#pragma warning restore CS8603 // Possible null reference return.
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+
+                // Get the highest bitrate audio-only stream
+                var streamInfo = streamManifest.GetAudioOnlyStreams().TryGetWithHighestBitrate();
+
+                // Download the stream to a file
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"audio.{streamInfo.Container}");
+                if (File.Exists($"audio.{streamInfo.Container}"))
+                {
+                    Console.WriteLine($"File {$"audio.{streamInfo.Container}"} downloaded successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"File {$"audio.{streamInfo.Container}"} was not downloaded.");
+                }
+                return $"audio.{streamInfo.Container}";
+                //return await youtube.Videos.Streams.GetAsync(streamInfo);
+
+
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return "";
+            }
         }
+
         static AudioOutStream? discordstream = null;
         public static async Task SendAsync(IAudioClient client, string path, CancellationToken cancellationToken)
         {
             try {
                 using (_currentProcess = CreateStream(path))
+                using (var output = _currentProcess.StandardOutput.BaseStream)
+                using (discordstream = client.CreatePCMStream(AudioApplication.Mixed))
+                {
+                    try { await output.CopyToAsync(discordstream, cancellationToken); }
+                    finally { await discordstream.FlushAsync(cancellationToken); }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("flushbug:" + e.Message);
+            }
+        }
+
+        public static async Task SendAsyncYT(IAudioClient client, string path, CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (_currentProcess = CreateStream(await CreateStreamYT(path)))
                 using (var output = _currentProcess.StandardOutput.BaseStream)
                 using (discordstream = client.CreatePCMStream(AudioApplication.Mixed))
                 {
